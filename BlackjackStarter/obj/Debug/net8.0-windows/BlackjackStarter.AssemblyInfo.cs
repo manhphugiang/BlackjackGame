a118be6abc500676c415afba43635e3dc9991b9c
giangma <giangma@sheridancollege.ca>
2024-06-04 09:21:15 -0400

    first commit

--- BlackjackStarter/obj/Debug/net8.0-windows/BlackjackStarter.AssemblyInfo.cs
+++ BlackjackStarter/obj/Debug/net8.0-windows/BlackjackStarter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlackjackStarter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+444aaf35c4a0f3b6c525b4c5ca4906f51dff638a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlackjackStarter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlackjackStarter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
